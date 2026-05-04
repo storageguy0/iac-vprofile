46bee3b94dc68361dc873e338e2d43dcb10fa820
storageguy0 <storageguy0@gmail.com>
2026-05-05 02:43:54 +0800

    "add eks token to main.tf v0.4"

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
 }
 
 provider "aws" {
