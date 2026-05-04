b24d184984dc56f2dc0d6ea2e80fab85326709c4
storageguy0 <storageguy0@gmail.com>
2026-05-05 02:04:38 +0800

    "modified according to the 2026 aws requirements V0.2"

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,28 +3,28 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
 #      version = "~> 5.25.0"
-      version = "~> 5.47.0"
+      version = "~> 5.85.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6.3"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.2.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3.7"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
 #      version = "~> 2.23.0"
-      version = "~> 2.31.0"
+      version = "~> 2.35.1"
     }
   }
 
