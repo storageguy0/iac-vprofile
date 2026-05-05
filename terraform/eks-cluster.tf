f0b986022344d692ecec213b7d63147ffe25ffcb
storageguy0 <storageguy0@gmail.com>
2026-05-06 01:48:28 +0800

    test workflow v0.6

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,43 +1,42 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.19.0"
+  version = "19.21.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.34"
+  name               = local.cluster_name
+  kubernetes_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # ✅ v21: 控制平面访问改到这里
   cluster_endpoint_public_access = true
 
-  # ✅ 权限
   enable_cluster_creator_admin_permissions = true
 
-  # ✅ v21: 名字变了
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
 
+  managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"   # 👈 改这里（关键）
+  }
+
+  managed_node_groups = {
+    one = {
       instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3
       desired_size = 2
-
-      ami_type = "AL2023_x86_64"
     }
 
     two = {
-      name = "node-group-2"
-
       instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 2
       desired_size = 1
-
-      ami_type = "AL2023_x86_64"
     }
   }
 }
